fafbcd0082217b993d81c5b2ba4c914b72dac913
akchi724 <akchi724@gmail.com>
2022-09-15 20:38:16 +0300

    1

--- src/main/java/org/example/ClassForTest.java
+++ src/main/java/org/example/ClassForTest.java
@@ -1,2 +1,8 @@
-package org.example;public class ClassForTest {
+package org.example;
+
+public class ClassForTest {
+
+    public String saySomething(String words){
+        return words + " 1";
+    }
 }
--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,10 +1,27 @@
 import org.example.ClassForTest;
+import org.junit.jupiter.api.Test;
 
-public class Test {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
 
-    private final ClassForTest task1 = new ClassForTest();
 
-    assertEquals(
-                "Vladimir",
-                task1.capitalize("vladimir"));
+public class TestExample {
+
+    private final ClassForTest classForTest = new ClassForTest();
+
+    @Test
+    public void oneWord() {
+        assertEquals(
+                "vladimir",
+                classForTest.saySomething("vladimir"));
+    }
+
+    @Test
+    public void oneWordAnd1() {
+        assertEquals(
+                "vladimir 1",
+                classForTest.saySomething("vladimir"));
+    }
 }
