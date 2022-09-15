a5dd77f654fe498bcf23b793b11d6affca8598bf
akchi724 <akchi724@gmail.com>
2022-09-15 16:00:53 +0300

    Initial commit

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,10 @@
+import org.example.ClassForTest;
+
+public class Test {
+
+    private final ClassForTest task1 = new ClassForTest();
+
+    assertEquals(
+                "Vladimir",
+                task1.capitalize("vladimir"));
+}
